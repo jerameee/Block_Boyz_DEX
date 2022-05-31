f1f93e8d4e42a22fa5f91587d5da1da4558b234a
Jeremy <jeremy@inavance.com>
2022-05-31 13:54:25 -0700

    commit on master

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { HiMenuAlt4 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
 
-import logo from '../../images/cb_logo_1.png';
+import logo from '../../images/cb_logo_2.png';
 
 const NavBarItem = ({ title, classprops }) => (
     <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
@@ -13,7 +13,7 @@ const [toggleMenu, setToggleMenu] = useState(false);
 return (
 <nav className="w-full flex md:justify-center justify-between items-center p-4">
     <div className="md:flex-[0.5] flex-initial justify-center items-center">
-        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+        <img src={logo} alt="logo" className="w-64 cursor-pointer" />
     </div>
     <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
         {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
